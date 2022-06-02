9fef05c4f1fee7e625638d76260ae2844aab4658
Matheus George <100322383+GGeorge777@users.noreply.github.com>
2022-06-02 11:08:38 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,26 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _4__ConversõesEOutrosTiposNumericos
+namespace _6__AplicaçõesDeVariaveis
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Executanto o projeto 4");
+            Console.WriteLine("Execução o projeto 6");
+            int idade = 32;
+            int idadeGustavo = idade;
 
-            double salario = 1200.43;
-            // o int é uma variavel de 32 bits
-            int salarioEmInteiro;
-            salarioEmInteiro = (int) salario;
-            Console.WriteLine(salarioEmInteiro);
-            // O long é uma variavel de 64 bits
-
-            long idade = 130000000000000000000;
-
-            short quantidadeProdutos = 15000;
-
-            float altura = 1.76f;
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGustavo);
 
             Console.ReadLine();
         }
