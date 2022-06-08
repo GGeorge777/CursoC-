7718f0b09cf26cf7e31a1f6d73abe0cc84efcdc4
Matheus George <100322383+GGeorge777@users.noreply.github.com>
2022-06-07 21:58:38 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,34 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExeEscopoIf
+namespace MultiplosDe3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // De 1900.0 até 2800.0, o IR é de 7.5% e pode deduzir na declaração o valor de R$ 142;
-            // De 2800.01 até 3751.0, o IR é de 15% e pode deduzir R$ 350;
-            // De 3751.01 até 4664.00, o IR é de 22.5% e pode deduzir R$ 636.
-            double salario = 3300.0;
-            if (salario >= 1900.0 && salario <= 2800.0)
+            for (int i = 1; i < 100; i++)
             {
-                Console.WriteLine("A sua aliquota é de 7.5%");
-                Console.WriteLine("Você pode deduzir até R$ 142");
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+               
             }
-            else if (salario >= 2800.01 && salario <= 3751.0)
-            {
-                Console.WriteLine("A sua aliquota é de  15%");
-                Console.WriteLine("Você pode deduzir até R$ 350");
-
-            }
-
-            else if (salario >= 3751.01 && salario <= 4664.0)
-            {
-                Console.WriteLine("A sua aliquota é de  22.5%");
-                Console.WriteLine("Você pode deduzir até R$ 636");
-            }
-            Console.ReadLine();
         }
     }
 }
+
